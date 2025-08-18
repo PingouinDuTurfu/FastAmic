ddd8cc96c51db1c42157aede14c99a96eb3026a6
PingouinDuTurfu <remy.ollivier64@gmail.com>
2025-08-19 01:45:58 +0200

    Update

--- app/services/invoice_service_2.py
+++ app/services/invoice_service_2.py
@@ -1,11 +1,8 @@
-from dependency_injector.wiring import inject
-
 from app.services.core.mysql_service import MySQLService
 
 
 class InvoiceService2:
 
-    @inject
     def __init__(self, mysql_service: MySQLService):
         self.mysql_service = mysql_service
 
